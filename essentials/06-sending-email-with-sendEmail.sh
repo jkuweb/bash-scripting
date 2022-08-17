31cfa19b6c43839bdd76a6901f6de5411818d82a
joseba <josebaquerejetaweb@gmail.com>
2022-08-17 17:55:57 +0200

    modified the files 06/07

--- essentials/06-sending-email-with-sendEmail.sh
+++ essentials/06-sending-email-with-sendEmail.sh
@@ -1,15 +1,14 @@
 #!/bin/bash 
 
 ## requirements
-## sendEmail && libraries -> apt-get install sendEmail libio-socket-ssl-perl libnet-ssleay-perl
+## sendEmail && libraries -> apt-get install sendemail libio-socket-ssl-perl libnet-ssleay-perl
 
 SMTPFROM=your_email@gmail.com
 SMTPTO=your_email@gmail.com
-SMTPSERVER=smtp.googlemail.com:587
+SMTPSERVER=smtp.googlemail.com:465
 SMTPUSER=user
 SMTPPASS=your_gmail_pass
 MESSAGEBODY="this is a test."
 SUBJECT="hello"
 
-sendEmail -f $SMTPFROM -t $SMTPTO -u $SUBJECT -m $MESSAGEBODY -s $SMTPSERVER -xu $SMTPUSER -xp $SMTPPASS
-
+sendEmail -f $SMTPFROM -t $SMTPTO -u $SUBJECT -m $MESSAGEBODY -s $SMTPSERVER -xu $SMTPUSER -xp $SMTPPASS -o tls=yes
--- essentials/07-sending-email-with-sendEmail-II.sh
+++ essentials/07-sending-email-with-sendEmail-II.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 ## requirements
-## sendEmail && libraries -> apt-get install sendEmail libio-socket-ssl-perl libnet-ssleay-perl
+## sendEmail && libraries -> apt-get install sendemail libio-socket-ssl-perl libnet-ssleay-perl
 
 SMTPFROM=your_email@gmail.com
 SMTPTO=your_email@gmail.com
-SMTPSERVER=smtp.googlemail.com:587
+SMTPSERVER=smtp.googlemail.com:465
 SMTPUSER=user
 SMTPPASS=your_gmail_pass
 SUBJECT="hello"
@@ -13,6 +13,6 @@ SUBJECT="hello"
 df -h | grep Filesystem > /temp/diskusage.txt
 df -h | grep sda1 >> /tmp/diskusage.txt
 
-sendEmail -f $SMTPFROM -t $SMTPTO -u $SUBJECT -o message-file=/tmp/diskusage.txt -s $SMTPSERVER -xu $SMTPUSER -xp $SMTPPASS
+sendEmail -f $SMTPFROM -t $SMTPTO -u $SUBJECT -o message-file=/tmp/diskusage.txt -s $SMTPSERVER -xu $SMTPUSER -xp $SMTPPASS -o tls=yes
 
 rm /tmp/diskusage.txt
